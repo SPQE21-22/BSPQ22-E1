abe03c7303002de9cdffa0b17ad8e35c79e9354e
EnekoEguiguren <71316809+Eneko9@users.noreply.github.com>
2022-04-03 12:36:38 +0200

    Delete create.sql

--- src/main/java/server/sql/create.sql
+++
@@ -1,8 +0,0 @@
--- file for schema and user creation
-DROP SCHEMA IF EXISTS libraryDB;
-DROP USER IF EXISTS 'spq'@'%';
-
-CREATE SCHEMA libraryDB;
-CREATE USER IF NOT EXISTS 'spq'@'%' IDENTIFIED BY 'spq';
-
-GRANT ALL ON libraryDB.* TO 'spq'@'%';
