cd9db68cee5433dacbba1ee799f179e8f9689ee9
EnekoEguiguren <eneko.eguiguren@opendeusto.es>
2022-04-21 12:07:02 +0200

    Testing started

---
+++ src/test/ServerTest.java
@@ -0,0 +1,18 @@
+
+import org.junit.Test;
+import java.server.sql.Server;
+
+public class ServerTest {
+    private Client client;
+    private WebTarget webTarget;
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testGetResp(){
+
+    }
+}
